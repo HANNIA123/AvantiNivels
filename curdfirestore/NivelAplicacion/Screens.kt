a9224b27ce201b2a1aa21cf08630f65b4f070092
HANNIA123 <69338022+HANNIA123@users.noreply.github.com>
2023-12-16 16:24:18 -0600

    Delete curdfirestore directory

--- curdfirestore/NivelAplicacion/Screens.kt
+++
@@ -1,9 +0,0 @@
-package com.example.curdfirestore.NivelAplicacion
-
-sealed class Screens(val route: String) {
-    //object MainScreen: Screens(route = "main_screen")
-    object GetDataScreen: Screens(route = "get_data_screen")
-    object AddDataScreen: Screens(route = "add_data_screen")
-    object Login: Screens(route = "login")
-
-}
